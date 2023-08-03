f5efc161fdf56bb5ccbc350954a7bcde06a02c7c
MarshmelloT <1716133138@qq.com>
2023-08-03 09:06:40 +0800

    修改

--- PastryMS/UI/Areas/Admin/Controllers/WorkFlow_ModelController.cs
+++ PastryMS/UI/Areas/Admin/Controllers/WorkFlow_ModelController.cs
@@ -4,6 +4,7 @@ using IBLL;
 using Microsoft.AspNetCore.Mvc;
 using Model;
 using Model.DTO;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +18,11 @@ namespace UI.Controllers
 
     public class WorkFlow_ModelController : Controller
     {
-        private RepositorySysDB _dbContext;
+        private PastryMSDB _dbContext;
         private IConsumableInfoBLL _consumableInfoBLL;
         private IWorkFlow_ModelBLL _workFlow_ModelBLL;
 
-        public WorkFlow_ModelController(RepositorySysDB dbContext
+        public WorkFlow_ModelController(PastryMSDB dbContext
             , IConsumableInfoBLL consumableInfoBLL
             , IWorkFlow_ModelBLL workFlow_ModelBLL
             )
@@ -63,7 +64,7 @@ namespace UI.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateWorkFlow_Model([FromBody] WorkFlow_Model entity)
+        public IActionResult CreateWorkFlow_Model([FromForm] WorkFlow_Model entity)
         {
             string msg;
             bool isok = _workFlow_ModelBLL.CreateWorkFlow_Model(entity, out msg);
@@ -80,6 +81,48 @@ namespace UI.Controllers
 
         }
 
+        [HttpPost]
+        public IActionResult DeleteWorkFlow_Model(string id)
+        {
+            ReturnResult result = new ReturnResult();
 
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                result.Msg = "id不能为空";
+                return Json(result);
+            }
+
+            bool isok = _consumableInfoBLL.DeleteConsumableInfo(id);
+
+            if (isok)
+            {
+                result.Code = 200;
+                result.Msg = "删除成功";
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteWorkFlow_Models(List<string> ids)
+        {
+            ReturnResult result = new ReturnResult();
+
+            if (ids == null || ids.Count == 0)
+            {
+                result.Msg = "你还没有选择要删除的id";
+                return Json(result);
+            }
+
+            bool isok = _consumableInfoBLL.DeleteConsumableInfos(ids);
+
+            if (isok)
+            {
+                result.Code = 200;
+                result.Msg = "删除成功";
+            }
+
+            return Json(result);
+        }
     }
 }
