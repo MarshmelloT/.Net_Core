b8d88f29e5df9345f832dc6dee76e2aaa901b763
MarshmelloT <1716133138@qq.com>
2023-08-03 10:53:58 +0800

    更新

---
+++ PastryMS/IBLL/IDessertInfoBLL.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBLL
+{
+    public interface IDessertInfoBLL
+    {
+        List<GetDessertInfoDTO> getDessertInfos(int page, int limit, string dessertName, string dessertTypeId, out int count);
+        bool UpLoadDessertInfo(DessertInfo entity, out string msg);
+        bool DownDessertInfo(DessertInfo dessertInfo, out string msg);
+        bool BuyDessertInfo(DessertInfo entity, out string msg);
+
+    }
+}
